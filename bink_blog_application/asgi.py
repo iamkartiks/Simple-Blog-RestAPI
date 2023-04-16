159496770baa079a943b125eb7075543773da544
iamkartiks <kartikeysinha40@gmail.com>
2023-04-16 14:27:54 +0530

    first-commit

---
+++ bink_blog_application/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for bink_blog_application project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bink_blog_application.settings')
+
+application = get_asgi_application()
